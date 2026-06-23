05a87e988609fd982d98edfdd902cd581cf40fc5
Gijs Dekker <125569460+Gijsde@users.noreply.github.com>
2026-06-23 14:25:43 +0200

    Enhance Neovim setup with new plugins and configs
    Added several new plugins for linting, completion, and formatting. Updated existing configurations for better functionality.

--- linux_dotfiles/nvim/init.lua
+++ linux_dotfiles/nvim/init.lua
@@ -1,22 +1,35 @@
--- initial setup for packages
 vim.pack.add {
 	"https://github.com/catppuccin/nvim",
     "https://github.com/neovim/nvim-lspconfig",
     "https://github.com/windwp/nvim-autopairs",
     "https://github.com/stevearc/oil.nvim",
     "https://github.com/ibhagwan/fzf-lua",
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" }
+    "https://github.com/mfussenegger/nvim-lint",
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+    "https://github.com/hrsh7th/nvim-cmp",
+    "https://github.com/hrsh7th/cmp-nvim-lsp",
+    "https://github.com/hrsh7th/cmp-buffer",
+    "https://github.com/numToStr/Comment.nvim",
+    "https://github.com/HiPhish/rainbow-delimiters.nvim",
+    "https://github.com/rktjmp/lush.nvim",
+    "https://github.com/vermdeep/darcula_dark.nvim",
+    "https://github.com/windwp/nvim-ts-autotag",
+    "https://github.com/stevearc/conform.nvim",
 }
 
 require("nvim-autopairs").setup()
+require('nvim-ts-autotag').setup()
 
 vim.cmd.colorscheme("catppuccin")
-
 
 -- keybindings
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-
+vim.keymap.set('n', '<leader>v', ':vsp<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true })
 
 -- vim settings
 vim.o.number = true
@@ -27,14 +40,35 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+vim.opt.scrolloff = 8;
+vim.opt.swapfile = false
 
 
 -- autocomplete
-vim.lsp.enable({ 
+vim.lsp.enable({
     "pyright",
-    "lua-lsp",
-    "clangd"
+    "lua_ls",
+    "clangd",
+    "ts_ls",
+    "html",
+    "cssls",
+    "jsonls",
+    "eslint",
 })
+
+-- Diagnostic display settings
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        source = true,
+    },
+})
+
+local diagnostics_enabled = true
 
 vim.o.autocomplete = true
 
@@ -48,32 +82,44 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
 })
 
+-- nvim-cmp setup
+local cmp = require('cmp')
+cmp.setup({
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    }),
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+})
+
 vim.opt.complete:append('o')
 vim.opt.completeopt = { 'menuone', 'noselect' }
 vim.opt.pumheight = 5
 
-vim.keymap.set('i', '<Tab>', function()
-    if vim.fn.pumvisible() == 1 then
-        return '<C-n>'
-    end
-    return '<Tab>'
-end, { expr = true })
+-- Comment.nvim setup
+require('Comment').setup()
 
-vim.keymap.set('i', '<S-Tab>', function()
-    if vim.fn.pumvisible() == 1 then
-        return '<C-p>'
-    end
-    return '<S-Tab>'
-end, { expr = true })
-vim.keymap.set('i', '<CR>', function()
-    if vim.fn.pumvisible() == 1 then
-        return '<C-y>'
-    end
-    return '<CR>'
-end, { expr = true })
+-- rainbow-delimiters setup
+require('rainbow-delimiters.setup').setup()
 
 -- Oil (file explorer)
-require('oil').setup()
+require('oil').setup({
+    view_options = {
+        show_hidden = true,
+    }
+}) 
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
 -- fzf-lua
@@ -93,6 +139,59 @@ end, { desc = 'Find buffers' })
 -- treesitter
 vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/site/pack/core/opt/nvim-treesitter')
 require('nvim-treesitter').setup({
-    ensure_installed = { "Python", "lua", "C", "Java", "javascript" },
+    ensure_installed = { "python", "lua", "c", "java", "javascript", "typescript", "tsx", "html", "css", "json" },
     highlight = { enable = true },
 })
+
+require('rainbow-delimiters.setup').setup({
+    strategy = {
+        [''] = require('rainbow-delimiters').strategy['global'],
+        vim = require('rainbow-delimiters').strategy['local'],
+        c =  require('rainbow-delimiters').strategy['local'],
+    },
+    query = {
+        [''] = 'rainbow-delimiters',
+        lua = 'rainbow-delimiters',
+        c = 'rainbow-delimiters',
+    },
+    highlight = {
+        'RainbowDelimiterRed',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterViolet',
+        'RainbowDelimiterCyan',
+    },
+})
+
+-- linting
+local lint = require('lint')
+
+lint.linters_by_ft = {
+  python     = { 'pylint' },
+  javascript = { 'eslint_d' },
+  typescript = { 'eslint_d' },
+  lua        = { 'luacheck' },
+  c          = { 'cppcheck' },
+}
+
+vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost' }, {
+  callback = function()
+    lint.try_lint()
+  end,
+})
+
+
+-- formatting
+require('conform').setup({
+    formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        json = { "prettier" },
+    },
+})
+vim.keymap.set('n', '<leader>f', function() require('conform').format() end, { desc = 'Format buffer' })
